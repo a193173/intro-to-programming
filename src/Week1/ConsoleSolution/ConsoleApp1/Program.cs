6a9597d938093d41a519164a5c7c21a14af2857a
Sharief M Reid <sharief_m_reid@progressive.com>
2023-09-18 15:58:58 -0400

    day 1

---
+++ src/Week1/ConsoleSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+
+string message = "Welcome to Class;";
+
+DateTime now = DateTime.Now;
+
+string finalMessage = $"The Message {message} and it is now {now:T}";
+
+var app = builder.Build();
+
+app.MapGet("/message", () =>
+{
+    return Results.Ok(finalMessage);
+});
+
+app.Run();
